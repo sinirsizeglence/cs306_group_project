f80f01e1a57cfb5fe41fbd9f235aa2fc624d4cb3
Ahmet Melih Afşar <afsarahmetmelih@gmail.com>
2023-05-19 18:17:51 +0300

    Add python plotter code for project step 4.

---
+++ python/connect.py
@@ -0,0 +1,25 @@
+# CODE BORROWED FROM:
+# https://github.com/hasan-ert/cs306-recit-materials/tree/main/Recitation%209
+
+
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+def connectionCreator():
+    try:
+        cnx = mysql.connector.connect(user="root", password="A.MelihAfsar67", database="proje")
+        print("Connection established with the database")
+        return cnx
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        return None
+    else:
+        cnx.close()
+        return None
